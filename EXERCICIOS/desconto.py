bfed04159c5c3e464b42a9a6c1e9bd5ee405070a
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-18 16:35:04 -0300

    att

--- EXERCICIOS/desconto.py
+++ EXERCICIOS/desconto.py
@@ -2,4 +2,4 @@ Preco = float(input('QUal o Preço do Produto? R$'))
 
 desc = Preco - (Preco*0.05)
 
-print(f'O Produto antes Custava {Preco} e agora com um desconto é {desc}')
+print(f'O Produto antes Custava {Preco} e agora com um desconto é {desc:.2f}')
